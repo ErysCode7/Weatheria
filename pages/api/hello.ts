de9cdc084829332519db03a8f1c17c2665a8f630
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2022-11-02 00:54:55 +0800

    fix: api routes

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json({ name: "Erys Mozo" });
+};
